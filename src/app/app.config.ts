4e235907271a7d3393dbe5205a7102e3d54f5346
Sanchana K <sanchanak2307@gmail.com>
2025-01-29 10:13:45 +0100

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,14 +7,7 @@ import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCuCEX_Y8Wcxx1TDk83vU8drceXGoUZNaA',
-  authDomain: 'netflix-9516a.firebaseapp.com',
-  projectId: 'netflix-9516a',
-  storageBucket: 'netflix-9516a.appspot.com',
-  messagingSenderId: '1078231927213',
-  appId: '1:1078231927213:web:6168026f06b1756728b924',
-};
+const firebaseConfig = {};
 
 export const appConfig: ApplicationConfig = {
   providers: [
